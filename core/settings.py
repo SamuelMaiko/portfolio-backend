cd17b6104279d25ee99bc85f4e235a3bd2ac9035
SamuelMaiko <sammymaiko721@gmail.com>
2025-07-09 15:03:26 +0300

    update

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
-DEBUG = False
+# DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "maikoportfolio.pythonanywhere.com"]
 
